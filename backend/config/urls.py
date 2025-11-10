3e3f5982e5dc58b20df9b43f1ec1960c6419372e
Carlos Ariel Diaz Cattoni <charlydiazcattoni@gmail.com>
2025-11-10 01:45:18 -0300

    establecimiento de urls variable envoirments

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,9 @@
 import os
 
-ENV = os.getenv("ENV", "development").strip() # development or production
-DEV_FRONTEND_URL = "http://localhost:5173"
-PROD_FRONTEND_URL = "https://unveneered-looped-josephine.ngrok-free.dev"
+# Determina si se está ejecutando en desarrollo o producción
+ENV = os.getenv("ENV", "development").strip()  # development or production
+
+# URLs del frontend y backend según el entorno
+DEV_FRONTEND_URL = os.getenv("DEV_FRONTEND_URL", "http://localhost:5173")
+PROD_FRONTEND_URL = os.getenv("PROD_FRONTEND_URL", "https://unveneered-looped-josephine.ngrok-free.dev")
 BACK_URL = "https://unveneered-looped-josephine.ngrok-free.dev"
